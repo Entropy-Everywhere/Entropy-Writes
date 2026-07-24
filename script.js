a233bb7724dae4fc8fd35dd95e1f38936a956895
EntropyEverywhere <commisioncloud@gmail.com>
2026-07-24 13:14:15 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,33 @@
+/* == Main == */
+
+fetch("/site/header.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("header").innerHTML = data;
+});
+
+fetch("/site/footer.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("footer").innerHTML = data;
+});
+
+fetch("/site/site-notice.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("sitenotice").innerHTML = data;
+});
+
+fetch("/site/sidebar.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("sidenav").innerHTML = data;
+});
